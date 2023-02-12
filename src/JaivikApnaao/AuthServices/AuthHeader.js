8b54e21cec432e903fbb7fffecac26afd5af312f
Siddhesh Patle <siddhup0109@gmail.com>
2023-02-12 18:47:02 +0530

    all changed

--- src/JaivikApnaao/AuthServices/AuthHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AuthHeader = () => {
-//   const user = JSON.parse(localStorage.getItem('user'));
-
-//   if(user)
-}
-
-export default AuthHeader
--- src/MyComponents/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer className='bg-dark text-light text-center py-2'>
-      <p>Copyright &copy; MyTodoList.com</p>
-    </footer>
-  )
-}
